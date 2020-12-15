9972da1a04b31cc5bbc19916e9d4d4cdb599390a
Baris Berkin Tasci <barisberkin.tasci@hotmail.com>
2020-12-15 12:04:42 +0300

    Creating and printing array.

---
+++ bbtExc1.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var numbers = Array(1...1000)    // creating an array that includes numbers 1,2, ... , 999, 1000
+
+for item in numbers {
+    print(item)                  // print elements of the array
+}
+
+print(numbers)                   // print the array
